d1ddb3f9bc580c784ea1556878d06afc28cfba32
Karolina147 <54249154+Karolina147@users.noreply.github.com>
2019-12-03 17:27:25 +0100

    Add files via upload

---
+++ timer2.js
@@ -0,0 +1,23 @@
+// var koniecegzaminu = 0;
+// var koniecczasu = 0;
+
+function odliczanie()
+{
+	var minuta = 60;
+	var sekunda = 0
+
+	if(minuta<=0&&sekunda<=0)
+	{ 
+		document.getElementById('zegar').innerHTML='Koniec czasu';
+	}
+
+	sekunda=sekunda-1;
+	if (sekunda<0) {sekunda=59; minuta=minuta-1;}
+	if (minuta<0) {minuta=59;}
+
+	if(minuta>=0&&sekunda>=0)
+	{
+		document.getElementById('zegar').innerHTML= 'Czas do zakończenia '+ minuta+' min '+ sekunda +' sek';
+		setTimeout("odliczanie()",1000);
+	} 
+}
