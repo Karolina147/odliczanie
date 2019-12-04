575a76b88d2fe68c6539343fe6c708e947f9bb3e
Karolina <arya0299@gmail.com>
2019-12-04 17:52:08 +0100

    counting issue fixed

--- timer2.js
+++ timer2.js
@@ -1,16 +1,15 @@
 // var koniecegzaminu = 0;
 // var koniecczasu = 0;
+var minuta = 60;
+var sekunda = 0;
 
 function odliczanie()
 {
-	var minuta = 60;
-	var sekunda = 0
-
 	if(minuta<=0&&sekunda<=0)
 	{ 
 		document.getElementById('zegar').innerHTML='Koniec czasu';
 	}
-
+	else {
 	sekunda=sekunda-1;
 	if (sekunda<0) {sekunda=59; minuta=minuta-1;}
 	if (minuta<0) {minuta=59;}
@@ -20,4 +19,5 @@ function odliczanie()
 		document.getElementById('zegar').innerHTML= 'Czas do zakończenia '+ minuta+' min '+ sekunda +' sek';
 		setTimeout("odliczanie()",1000);
 	} 
+}
 }
